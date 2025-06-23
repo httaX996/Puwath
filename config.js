8b3ff77c000b670f69feba5bbd570af6cb75369c
Usbswu <jakabik2@gmail.com>
2025-06-23 11:35:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9nFVSBpD#bTGYxNjB9ONeNt6sdRA-eSYm-vSvvbv2Re6l8Af0F60",
+SESSION_ID: process.env.SESSION_ID || "xy8QVajb#2M1TgXvGOiBp9WN5soKcQqnCCnTY3oA4yhD1zaOEaGk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
